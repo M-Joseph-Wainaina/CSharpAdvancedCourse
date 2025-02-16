9bfa7deb5c19b78f77de360d09be0e19c18dca97
Wainaina <ynainajoseph@gmail.com>
2025-02-16 17:27:55 +0300

    csharp crash course clr architecture, variables, overflowing, scope of a variable, variable types and conversions, operaators(assignment, comparison, logical, arithematic ..)

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////implicit type conversion
+            //byte a  = 3;
+            //int b = a;
+
+            ////explicit type conversion
+
+            //int c = 3;
+            //byte d = (byte)c;
+
+            ////using the Convert class library
+
+            //string e = "3";
+            //int f = Convert.ToInt32(e);
+            //int g = int.Parse(e);
+
+            //Console.WriteLine(b);
+            //Console.WriteLine(d);
+            //Console.WriteLine(g);
+
+            ////handling exceptions 
+            //try
+            //{
+            //    string number = "1234";
+            //    byte a = Convert.ToByte(number);
+            //    Console.WriteLine(a);
+            //}
+            //catch (Exception)
+            //{
+
+            //    Console.WriteLine("Cannot convert the number to a byte");
+            //}
+
+            //arithematic operators(+, -, *, / , %)
+
+             int a = 1;
+             int b = 2;
+             int c = 3; 
+
+
+            Console.WriteLine(c> a && c > b);
+    
+                
+
+            
+        }
+    }
+}
